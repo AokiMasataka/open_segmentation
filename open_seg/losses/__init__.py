69d0c0411a5b50e44b0b0675a79cd1ac4fba7819
AokiMasataka <4okimasataka@gmail.com>
2022-07-22 21:40:51 +0900

    add lovasz loss

--- open_seg/losses/__init__.py
+++ open_seg/losses/__init__.py
@@ -1,2 +1,4 @@
 from .dice_loss import DiceLoss, dice_metric
 from .cross_entropy_loss import CrossEntropyLoss
+from .lovasz_loss import LovaszHingeLoss
+from .deep_supervision import lovasz_hinge_non_empty
---
+++ open_seg/losses/lovasz_loss.py
@@ -0,0 +1,117 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional
+from torch.autograd import Variable
+from itertools import filterfalse
+from open_seg.builder import LOSSES
+
+
+@LOSSES.register_module
+class LovaszHingeLoss(nn.Module):
+    def __init__(self, per_image=True, ignore=None, loss_weight=1.0, loss_name='lovasz_hinge'):
+        super().__init__()
+        self.per_image = per_image
+        self.ignore = ignore
+        self.loss_weight = loss_weight
+        self.loss_name = loss_name
+
+        if per_image:
+            self.loss_fn = self._per_image
+        else:
+            self.loss_fn = self._not_per_image
+
+    def forward(self, logits, labels):
+        return self.loss_fn(logits=logits, labels=labels) * self.loss_weight
+
+    def _per_image(self, logits, labels):
+        return mean(lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), self.ignore)) for log, lab in zip(logits, labels))
+
+    def _not_per_image(self, logits, labels):
+        return lovasz_hinge_flat(*flatten_binary_scores(logits, labels, self.ignore))
+
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1.0 - intersection / union
+    if p > 1:
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+
+def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        loss = mean(lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore)) for log, lab in zip(logits, labels))
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.0
+    signs = 2.0 * labels.float() - 1.0
+    errors = (1.0 - logits * Variable(signs))
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = lovasz_grad(gt_sorted)
+    loss = torch.dot(functional.relu(errors_sorted), Variable(grad))
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.view(-1)
+    labels = labels.view(-1)
+    if ignore is None:
+        return scores, labels
+    valid = (labels != ignore)
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    return vscores, vlabels
+
+
+def mean(l, ignore_nan=False, empty=0):
+    """
+    nanmean compatible with generators.
+    """
+    l = iter(l)
+    if ignore_nan:
+        l = filterfalse(np.isnan, l)
+    try:
+        n = 1
+        acc = next(l)
+    except StopIteration:
+        if empty == 'raise':
+            raise ValueError('Empty mean')
+        return empty
+    for n, v in enumerate(l, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
