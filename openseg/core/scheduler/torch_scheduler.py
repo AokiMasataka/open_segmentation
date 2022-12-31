99439ff0b4c850cc12403c73801fc078b85d53e3
AokiMasataka <4okimasataka@gmail.com>
2022-12-31 20:57:19 +0900

    remove timm scheduler

--- openseg/core/scheduler/torch_scheduler.py
+++ openseg/core/scheduler/torch_scheduler.py
@@ -1,6 +1,6 @@
 from inspect import getmembers, isclass
 from torch.optim import lr_scheduler
-from timm import scheduler
+# from timm import scheduler
 from ..builder import SCHEDULERS
 
 
@@ -14,7 +14,7 @@ class DummyScheduler:
 
 
 members = getmembers(object=lr_scheduler, predicate=isclass)
-members += getmembers(object=scheduler, predicate=isclass)
+# members += getmembers(object=scheduler, predicate=isclass)
 
 for member in members:
     SCHEDULERS.register_module(module=member[1], module_name=None)
